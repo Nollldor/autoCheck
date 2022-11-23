3ecee4c82ecb7038d0d1edb85a333f71c0e51d99
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-11-23 17:08:58 +0300

    12 select theme

--- hws2-master/src/s2-homeworks/hw12/bll/themeReducer.ts
+++ hws2-master/src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,11 +1,20 @@
+import {loadingAC} from "../../hw10/bll/loadingReducer";
+
+export type StateType = {
+    themeId: number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type ActionsType = ReturnType<typeof changeThemeId>
+
+export const themeReducer = (state = initState, action: ActionsType): StateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
         default:
             return state
     }
